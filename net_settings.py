145b88f9c2795c0c46cb3075f89b1749cd7b0c81
jczigany <jczigany@infomagus.hu>
2021-04-08 14:54:03 +0200

    Az automata játék kivételével kész config.ini a hálózati beállításokhoz, adatbázissal szinkronizálva van

--- net_settings.py
+++ net_settings.py
@@ -7,13 +7,13 @@ db = QSqlDatabase.addDatabase('QSQLITE')
 db.setDatabaseName('scorer.db3')
 config = configparser.ConfigParser()
 #Ez nem kell majd, ha a mainwindow-ból hívom meg
-if not db.open():
-    QMessageBox.critical(
-        None,
-        "App Name - Error!",
-        "Database Error: %s" % db.lastError().text(),
-    )
-    sys.exit(1)
+# if not db.open():
+#     QMessageBox.critical(
+#         None,
+#         "App Name - Error!",
+#         "Database Error: %s" % db.lastError().text(),
+#     )
+#     sys.exit(1)
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -32,15 +32,18 @@ class NetworkSettingsDialog(QDialog):
         adatok = QGridLayout()
         self.layout.addLayout(adatok)
 
+
+
         self.ip = QLineEdit()
         self.token = QLineEdit()
         self.station = QLineEdit()
+        self.valtozott = False
+        self.uj_station = False
 
         self.is_exist_config()
         self.check_config()
 
         adatok.addWidget(QLabel("IP cím: "), 0, 0)
-        # self.ip.setText(get_ip_address())
         self.ip.setDisabled(True)
         adatok.addWidget(self.ip, 0, 1)
 
@@ -50,6 +53,7 @@ class NetworkSettingsDialog(QDialog):
         adatok.addWidget(QLabel("Állomás azonosító: "), 2, 0)
 
         self.station.setDisabled(True)
+        self.station.editingFinished.connect(self.change_name)
         adatok.addWidget(self.station, 2, 1)
         self.space = QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
         self.layout.addItem(self.space)
@@ -71,31 +75,32 @@ class NetworkSettingsDialog(QDialog):
 
             station_id = config['DEFAULT'].get('station id')
             if (station_id is None) or len(station_id) < 4:
+                self.valtozott = True
                 msg = QMessageBox()
                 msg.setStyleSheet("fonz-size: 20px")
                 msg.setWindowTitle("Hibás állomás név!")
                 msg.setText(
-                    '<html style="font-size: 14px; color: red">Nem megfelelő állomás-azonosító!<br></html>' + '<html style="font-size: 16px">Kérem módosítsa a beállításokat!</html')
+                    '<html style="font-size: 14px; color: red">Nem megfelelő állomás-azonosító!<br></html>' + '<html style="font-size: 16px">Kérem módosítsa a beállításokat!</html>')
                 msg.exec_()
                 station = secrets.token_hex(8)
                 config.set('DEFAULT', 'station id', station)
                 self.station.setText(station)
-            # else:
-            #     self.station.setText(station_id)
 
             ip_config = config['DEFAULT'].get('station ip')
             if ip_fizikai != ip_config:
+                self.valtozott = True
                 msg = QMessageBox()
                 msg.setStyleSheet("fonz-size: 20px")
                 msg.setWindowTitle("Hibás beállítás!")
                 msg.setText(
-                    '<html style="font-size: 14px; color: red">A fizikai IP cím eltér a konfigurációtól!<br></html>' + '<html style="font-size: 16px">Kérem módosítsa a beállításokat!</html')
+                    '<html style="font-size: 14px; color: red">A fizikai IP cím eltér a konfigurációtól!<br></html>' + '<html style="font-size: 16px">Kérem módosítsa a beállításokat!</html>')
                 msg.exec_()
                 self.ip.setText(ip_fizikai)
                 config.set('DEFAULT', 'station ip', ip_fizikai)
 
             secret = config['DEFAULT'].get('secret key')
             if  (secret is None) or len(secret) != 32:
+                self.valtozott = True
                 newsecret = secrets.token_hex(16)
                 config.set('DEFAULT', 'secret key', newsecret)
                 self.token.setText(newsecret)
@@ -105,11 +110,12 @@ class NetworkSettingsDialog(QDialog):
 
         else:
             # Nincs config.ini, alapértékekkel inicializálni
+            self.valtozott = True
             msg = QMessageBox()
             msg.setStyleSheet("fonz-size: 20px")
             msg.setWindowTitle("Hiányzó beállítás file!")
             msg.setText(
-                '<html style="font-size: 14px; color: red">Nem tudtam beolvasni a konfigurációt!<br></html>' + '<html style="font-size: 16px">Kérem módosítsa a beállításokat!</html')
+                '<html style="font-size: 14px; color: red">Nem tudtam beolvasni a konfigurációt!<br></html>' + '<html style="font-size: 16px">Kérem módosítsa a beállításokat!</html>')
             msg.exec_()
             kulcs = secrets.token_hex(16)
             station = secrets.token_hex(8)
@@ -131,22 +137,21 @@ class NetworkSettingsDialog(QDialog):
         self.token.setText(secret)
         # todo Ellenőrizni, hogy van-e, és mi van a db-ben
         model2 = QSqlQueryModel()
-        query = QSqlQuery(f"SELECT * FROM reged_station where station_id = '{station_id}' or station_ip = '{ip_config}'", db=db)
+        # query = QSqlQuery(f"SELECT * FROM reged_station where station_id = '{station_id}' and station_ip = '{ip_config}'", db=db)
+        query = QSqlQuery(f"SELECT * FROM reged_station where secret_key = '{secret}'", db=db)
         model2.setQuery(query)
+        print(model2.record(0), model2.record(0).value(0))
         if model2.record(0).value(0):
-            print("van")
-        # else:
-        #     model1 = QSqlTableModel()
-        #     model1.setTable("reged_station")
-        #     rec = model1.record()
-        #     rec.remove(0)
-        #     rec.setValue(0, station_id)
-        #     rec.setValue(1, ip_config)
-        #     rec.setValue(2, secret)
-        #     if model1.insertRecord(-1, rec):
-        #         model1.submitAll()
-        #     else:
-        #         db.rollback()
+            if self.station.text() != model2.record(0).value(1) or self.ip.text() != model2.record(0).value(2) or self.token.text() != model2.record(0).value(3):
+                self.valtozott = True
+                self.rec_id = model2.record(0).value(0)
+                print("változott")
+        else:
+            self.uj_station = True
+            print("Új állomás")
+
+    def change_name(self):
+        self.valtozott = True
 
     def buttonbox_click(self, b):
         print(b.text())
@@ -162,40 +167,45 @@ class NetworkSettingsDialog(QDialog):
         self.station.setDisabled(False)
 
     def accept(self):
-        network = QSqlTableModel()
-        network.setTable("reged_station")
-        rec_net = network.record()
-        rec_net.remove(0)
-        rec_net.setValue(0, self.station.text())
-        rec_net.setValue(1, self.ip.text())
-        if network.insertRecord(-1, rec_net):
-            network.submitAll()
-        else:
-            db.rollback()
+        if self.uj_station:
+            network = QSqlTableModel()
+            network.setTable("reged_station")
+            rec_net = network.record()
+            rec_net.remove(0)
+            rec_net.setValue(0, self.station.text())
+            rec_net.setValue(1, self.ip.text())
+            rec_net.setValue(2, self.token.text())
+            if network.insertRecord(-1, rec_net):
+                network.submitAll()
+            else:
+                db.rollback()
+            self.uj_station = False
+
+        if self.valtozott:
+            # Módosítani a db-t és a config.ini-t
+            # config.ini update
+            config.set('DEFAULT', 'secret key', self.token.text())
+            config.set('DEFAULT', 'station ip', self.ip.text())
+            config.set('DEFAULT', 'station id', self.station.text())
+            with open('config.ini', 'w') as configfile:
+                config.write(configfile)
+            # db update
+            model3 = QSqlQueryModel()
+            query3 = QSqlQuery(f"update reged_station set station_id='{self.station.text()}', station_ip='{self.ip.text()}' where secret_key='{self.token.text()}'", db=db)
+            model3.setQuery(query3)
+            self.valtozott = False
         super().accept()
 
     def reject(self):
-        super().reject()
-
-# config['DEFAULT'] = {
-#     'Station IP' : '192.168.68.3',
-#     'Station Id' : 'tabla3'
-# }
-# config['MATCH_DEFAULTS'] = {
-#     'Player 1' : 'Player_1',
-#     'Player 2' : 'Player_2',
-#     'Variant' : '501',
-#     'Leg Number' : 3
-# }
-# with open('config.ini', 'w') as configfile:
-#     config.write(configfile)
-
-
-# print(config['DEFAULT'])
-# for key in config['DEFAULT']:
-#     print(key, config['DEFAULT'][key])
-#
-# print(config['DEFAULT'].get('station id',))
+        # Ha valami db művelet kell
+        if self.valtozott or self.uj_station:
+            msg = QMessageBox()
+            msg.setStyleSheet("fonz-size: 20px")
+            msg.setWindowTitle("Regisztrációs eltérés!")
+            msg.setText('<html style="font-size: 14px; color: red">Kérem módosítás után mentse a beállításokat!<br></html>')
+            msg.exec_()
+        else:
+            super().reject()
 
 if __name__ == '__main__':
     app = QApplication([])
