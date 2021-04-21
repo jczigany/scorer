8d0fbbea97745d5b698f36d549cd6e147185b1f2
jczigany <jczigany@infomagus.hu>
2021-04-21 19:57:48 +0200

    gamesettiongs-ben todo-k beírása

--- gamesettings2.py
+++ gamesettings2.py
@@ -19,20 +19,10 @@ class GameSettingsDialog(QDialog):
         self.parent = parent
         self.setModal(True)
         self.setWindowTitle("Game settings")
+        self.create_widgets()
+        self.set_layouts()
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-        self.kontener_names = QHBoxLayout()
-        self.kontener_buttons = QHBoxLayout()
-        self.kontener_szovegek1 = QVBoxLayout()
-        self.kontener_sets = QHBoxLayout()
-        self.kontener_handi = QHBoxLayout()
-        self.layout.addLayout(self.kontener_names)
-        self.layout.addLayout(self.kontener_szovegek1)
-        self.layout.addLayout(self.kontener_buttons)
-        self.layout.addLayout(self.kontener_sets)
-        self.layout.addLayout(self.kontener_handi)
-
+    def create_widgets(self):
         self.label_player1 = QLabel("Player 1")
         self.input_player1_name = QLineEdit()
         self.input_player1_name.setPlaceholderText("Player 1")
@@ -45,27 +35,14 @@ class GameSettingsDialog(QDialog):
         self.input_player2_name.setPlaceholderText("Player 2")
         self.player2_completer = QCompleter()
         self.input_player2_name.setCompleter(self.player2_completer)
-
+        # player widget-ek feltültése a db-ben szereplő nevekkel, autocomplete-hez
         self.get_player_name()
-
-        self.kontener_names.addWidget(self.label_player1)
-        self.kontener_names.addWidget(self.input_player1_name)
-        self.kontener_names.addWidget(self.label_player2)
-        self.kontener_names.addWidget(self.input_player2_name)
-
-        self.kontener_szovegek1.addWidget(QLabel("Leave Player 2 blank for single player"))
-        self.kontener_szovegek1.addWidget(QLabel("Variant"))
 
         self.gomb_301 = QRadioButton("301")
         self.gomb_401 = QRadioButton("401")
         self.gomb_501 = QRadioButton("501")
         self.gomb_501.setChecked(True)
         self.gomb_701 = QRadioButton("701")
-
-        self.kontener_buttons.addWidget(self.gomb_301)
-        self.kontener_buttons.addWidget(self.gomb_401)
-        self.kontener_buttons.addWidget(self.gomb_501)
-        self.kontener_buttons.addWidget(self.gomb_701)
 
         self.spin_legs = QSpinBox()
         self.spin_legs.setValue(3)
@@ -77,11 +54,6 @@ class GameSettingsDialog(QDialog):
         self.spin_sets.setMinimum(1)
         self.spin_sets.setMaximum(15)
 
-        self.kontener_sets.addWidget(QLabel("Legs per set: "))
-        self.kontener_sets.addWidget(self.spin_legs)
-        self.kontener_sets.addWidget(QLabel("Number os sets: "))
-        self.kontener_sets.addWidget(self.spin_sets)
-
         self.handi1 = QSpinBox()
         self.handi1.setValue(0)
         self.handi1.setMinimum(-100)
@@ -92,13 +64,46 @@ class GameSettingsDialog(QDialog):
         self.handi2.setMinimum(-100)
         self.handi2.setMaximum(100)
 
+        self.buttonbox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel | QDialogButtonBox.Reset)
+        self.buttonbox.clicked.connect(self.buttonbox_click)
+
+    def set_layouts(self):
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.kontener_names = QHBoxLayout()
+        self.kontener_names.addWidget(self.label_player1)
+        self.kontener_names.addWidget(self.input_player1_name)
+        self.kontener_names.addWidget(self.label_player2)
+        self.kontener_names.addWidget(self.input_player2_name)
+
+        self.kontener_buttons = QHBoxLayout()
+        self.kontener_buttons.addWidget(self.gomb_301)
+        self.kontener_buttons.addWidget(self.gomb_401)
+        self.kontener_buttons.addWidget(self.gomb_501)
+        self.kontener_buttons.addWidget(self.gomb_701)
+
+        self.kontener_szovegek1 = QVBoxLayout()
+        self.kontener_szovegek1.addWidget(QLabel("Leave Player 2 blank for single player"))
+        self.kontener_szovegek1.addWidget(QLabel("Variant"))
+
+        self.kontener_sets = QHBoxLayout()
+        self.kontener_sets.addWidget(QLabel("Legs per set: "))
+        self.kontener_sets.addWidget(self.spin_legs)
+        self.kontener_sets.addWidget(QLabel("Number os sets: "))
+        self.kontener_sets.addWidget(self.spin_sets)
+
+        self.kontener_handi = QHBoxLayout()
         self.kontener_handi.addWidget(QLabel("Handicap (1)"))
         self.kontener_handi.addWidget(self.handi1)
         self.kontener_handi.addWidget(QLabel("Handicap (2)"))
         self.kontener_handi.addWidget(self.handi2)
 
-        self.buttonbox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel | QDialogButtonBox.Reset)
-        self.buttonbox.clicked.connect(self.buttonbox_click)
+        self.layout.addLayout(self.kontener_names)
+        self.layout.addLayout(self.kontener_szovegek1)
+        self.layout.addLayout(self.kontener_buttons)
+        self.layout.addLayout(self.kontener_sets)
+        self.layout.addLayout(self.kontener_handi)
         self.layout.addWidget(self.buttonbox)
 
     def get_player_name(self):
@@ -132,11 +137,9 @@ class GameSettingsDialog(QDialog):
         m_id = p1_id = p2_id = set = leg = hc1 = hc2 =0
         var = ""
         player1 = self.input_player1_name.text()
-        print(player1, len(player1))
         player2 = self.input_player2_name.text()
-        # A MATCH_ID-T AZ AUTOINCREMENTBŐL KELLENE VISSZAKÉRNI ÉS NEM IMPLICIT RANDOMBÓL GENERÁLNI
+        # todo A MATCH_ID-T VALAMI EGYEDI MÓDON KELL GENERÁLNI(pl. az időbélyeg bizonyos részével)
         m_id = random.randint(10, 1000000)
-        # print(m_id)
         leg = self.spin_legs.value()
         set = self.spin_sets.value()
         hc1 = self.handi1.value()
@@ -157,6 +160,7 @@ class GameSettingsDialog(QDialog):
             player1_id_model = QSqlQueryModel()
             query1 = QSqlQuery(f"SELECT player_id FROM players where player_name = '{player1}' and type='local' and aktiv=1", db=db)
             player1_id_model.setQuery(query1)
+            # todo megnézni, hogy sima query.exec_ -el hogyan működik, lehet-e ellenőrizni, hogy üres vagy nem
             if player1_id_model.record(0).value(0):
                 p1_id = int(player1_id_model.record(0).value(0))
             else:
@@ -174,6 +178,7 @@ class GameSettingsDialog(QDialog):
                     db.rollback()
                 query1 = QSqlQuery(f"SELECT player_id FROM players where player_name = '{player1}' and type='local' and aktiv=1", db=db)
                 player1_id_model.setQuery(query1)
+                # todo megnézni, hogy sima query.exec_ -el hogyan működik, lehet-e ellenőrizni, hogy üres vagy nem
                 p1_id = int(player1_id_model.record(0).value(0))
 
         if len(player2) == 0:
@@ -183,6 +188,7 @@ class GameSettingsDialog(QDialog):
             player2_id_model = QSqlQueryModel()
             query2 = QSqlQuery(f"SELECT player_id FROM players where player_name = '{player2}' and type='local' and aktiv=1", db=db)
             player2_id_model.setQuery(query2)
+            # todo megnézni, hogy sima query.exec_ -el hogyan működik, lehet-e ellenőrizni, hogy üres vagy nem
             if player2_id_model.record(0).value(0):
                 p2_id = int(player2_id_model.record(0).value(0))
             else:
@@ -199,9 +205,11 @@ class GameSettingsDialog(QDialog):
                     db.rollback()
                 query2 = QSqlQuery(f"SELECT player_id FROM players where player_name = '{player2}' and type='local' and aktiv=1", db=db)
                 player2_id_model.setQuery(query2)
+                # todo megnézni, hogy sima query.exec_ -el hogyan működik, lehet-e ellenőrizni, hogy üres vagy nem
                 p2_id = int(player2_id_model.record(0).value(0))
 
         # Match paremeterek rögzítése
+        # todo a táblát kiagészíteni a handic. értékekkel
         now = QDateTime.currentDateTime()
         match_model = QSqlTableModel()
         match_model.setTable("match_settings")
@@ -213,7 +221,6 @@ class GameSettingsDialog(QDialog):
         record.setValue(4, leg)
         record.setValue(5, set)
         record.setValue(6, now)
-        print(record)
         if match_model.insertRecord(-1, record):
             match_model.submitAll()
         else:
@@ -232,8 +239,6 @@ class GameSettingsDialog(QDialog):
         self.parent.new_game_window.refresh()
         super().accept()
 
-
     def reject(self):
-        print("CANCEL")
         self.parent.new_game_window.close()
         super().reject()
